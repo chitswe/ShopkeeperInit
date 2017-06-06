c40d435a0a34b3da18207fe4f5dc4112c17d45e8
chitswe <chitswe.mdy@gmail.com>
2017-06-06 17:17:04 +0630

    Rename pic files

---
+++ backup.js
@@ -0,0 +1,67 @@
+import cloudinary from 'cloudinary';
+import pictures from './pictures';
+import fs from 'fs';
+import jsonfile from 'jsonfile';
+
+cloudinary.config({ 
+  cloud_name: 'shopkeeper-mt', 
+  api_key: '156887998232998', 
+  api_secret: 'XI0icVrIctxGn9P7Z8pEJU0b49A' 
+});
+
+
+const fileName = "./pictures.json";
+
+jsonfile.readFile(fileName,(err,images)=>{
+	let promises = images.slice(0,3).map((i,index)=>{
+		return new Promise((r,j)=>{
+			let {pic,format,completed} = i;
+			if(completed)
+				r(true);
+			else{
+				cloudinary.uploader.upload(`./pic/${pic}.${format}`,
+					result=>{
+						if(result.error){
+							console.log(`File "${pic}" is failed.`);
+							console.log(result);
+							console.log("................");
+							r(result);
+						}else{
+							i.completed=true;
+							images[index] = i;
+							console.log(`File "${pic}" is completed.`);
+							console.log(result);
+							console.log("................");
+							r(result);
+						}
+					},
+					{
+						public_id:`shopkeeper/product/${pic}`,
+						tags:['Shopkeeper', 'immoderate']
+					}
+				);
+			}
+		});
+	});
+	Promise.all(promises).then(result=>{
+		console.log("All completed.");
+	})
+	.catch(error=>{
+		console.log(error);
+	}).then(()=>{
+		jsonfile.writeFile(fileName,images,{space:2},err=>{
+			if(err){
+				console.log(err);
+			}else{
+				console.log("Progress saved!.");
+			}
+		});
+	});
+})
+
+
+
+
+
+
+
---
+++ rename.js
@@ -0,0 +1,45 @@
+import pictures from './pictures';
+import fs from 'fs';
+
+
+
+fs.readdir('./pic/',(err,files)=>{
+	files.forEach(f=>{
+		let split= f.split('_')[0];
+		if(!isNaN(split)){
+			fs.rename(`./pic/${f}`,`./pic/${f.replace(split + '_','')}`);
+		}
+	});
+});
+
+
+
+
+
+let promises = pictures.map(pic=>{
+	return new Promise(resolve=>{
+		fs.exists(`./pic/${pic}.png`,e=>{
+			if(e){
+				resolve({pic,format:'png'});
+			}
+			else{
+				fs.exists(`./pic/${pic}.jpeg`,ee=>{
+					if(ee){
+						resolve({pic,format:"jpeg"});
+					}
+					else{
+						resolve({pic,format:false});
+					}
+				});
+			}
+		});
+	});
+});
+
+Promise.all(promises).then(result=>{
+	console.log(result);
+})
+
+
+
+
