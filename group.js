38bdcab9cbdbf85a6947392360dbf18f83d1477f
chitswe <chitswe.mdy@gmail.com>
2017-06-08 11:44:11 +0630

    group pic

---
+++ group.js
@@ -0,0 +1,66 @@
+import cloudinary from 'cloudinary';
+import fs from 'fs';
+import jsonfile from 'jsonfile';
+
+cloudinary.config({ 
+  cloud_name: 'shopkeeper-mt', 
+  api_key: '156887998232998', 
+  api_secret: 'XI0icVrIctxGn9P7Z8pEJU0b49A' 
+});
+
+
+const fileName = "./group.json";
+
+jsonfile.readFile(fileName,(err,images)=>{
+	let promises = images.map((i,index)=>{
+		return new Promise((r,j)=>{
+			let {pic,format,completed} = i;
+			if(completed)
+				r(true);
+			else{
+				cloudinary.uploader.upload(`./pic/${pic}.${format}`,
+					result=>{
+						if(result.error){
+							console.log(`File "${pic}" is failed.`);
+							console.log(result);
+							console.log("................");
+							r(result);
+						}else{
+							i.completed=true;
+							images[index] = i;
+							console.log(`File "${pic}" is completed.`);
+							console.log(result);
+							console.log("................");
+							r(result);
+						}
+					},
+					{
+						public_id:`shopkeeper/productgroup/${pic}`,
+						tags:['Shopkeeper', 'immoderate']
+					}
+				);
+			}
+		});
+	});
+	Promise.all(promises).then(result=>{
+		console.log("All completed.");
+	})
+	.catch(error=>{
+		console.log(error);
+	}).then(()=>{
+		jsonfile.writeFile(fileName,images,{space:2},err=>{
+			if(err){
+				console.log(err);
+			}else{
+				console.log("Progress saved!.");
+			}
+		});
+	});
+})
+
+
+
+
+
+
+
